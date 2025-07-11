d68c7a133f77585fc05546ee539599ef077050b1
John Camino <jal.cmno@gmail.com>
2025-07-11 14:57:15 -0700

    Added a NavBar file to store all nav bar components. Also I cleared the App.css App.jsx index.css files to test my page.

---
+++ src/Tripinary/src/pages/home-page/NavBar/navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className = 'navbar'>
+      <h1>Tripinary</h1>
+    </div>
+  )
+}
+
+export default navbar
