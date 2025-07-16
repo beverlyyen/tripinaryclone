121efa2b725739e397deb037cc2daddb43ff2b29
beverlyyen <beverly.yen@gmail.com>
2025-07-16 11:20:52 -0700

    added basic css

--- src/Tripinary/src/pages/slide-out/slideout.jsx
+++ src/Tripinary/src/pages/slide-out/slideout.jsx
@@ -42,45 +42,43 @@ function SidePanel({ isOpen }) {
   return (
     <div className={`side-panel-full ${isOpen ? 'open' : ''}`}>
       <div className="inner-panel">
-        <div className="search-bar">
-          <input type="text" placeholder="search places!!!" />
-          <span className="search-icon">search</span>
+        <div className="left-panel">
+          <div className="search-bar">
+            <input type="text" placeholder="search places!!!" />
+            <span className="search-icon">search</span>
+          </div>
+          <iframe
+            className="map-placeholder"
+            src="https://maps.google.com/maps?q=Simon+Fraser+University&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            allowFullScreen
+            loading="lazy"
+            title="Map"
+          ></iframe>
         </div>
 
-        <iframe
-          className="map-placeholder"
-          src="https://maps.google.com/maps?q=Simon+Fraser+University&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          allowFullScreen
-          loading="lazy"
-          title="Map"
-        ></iframe>
-      </div>
-      <div className="right-panel">
-
-
-
-
-        <div className="info-card">  {/* use sfu loc for now*/}
-          <div className="info-text">
-            <h3>Simon Fraser University</h3>
-            <p>☆☆☆☆☆</p>  {/* put in revs from google api */}
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-          </div>
-          <div className="image-placeholder"><img src={duckImage}></img></div>
-        </div>
-
-        <div className="review-card">
-          <h4>User Reviews</h4>
-          <div className="review-scroll">
-            {reviews.map((review, i) => (
-              <div className="review" key={i}>
-                <p>{review.name}{'★'.repeat(review.stars)}{'☆'.repeat(5 - review.stars)}</p>
-                <p>{review.date}</p>
-                <p>{review.text}</p>
-              </div>
-            ))}
+        <div className="right-panel">
+          <div className="info-card">  {/* use sfu loc for now*/}
+            <div className="info-text">
+              <h3>Simon Fraser University</h3>
+              <p>☆☆☆☆☆</p>  {/* put in revs from google api */}
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </div>
+            <div className="image-placeholder"><img src={duckImage}></img></div>
           </div>
 
+          <div className="review-card">
+            <h4>User Reviews</h4>
+            <div className="review-scroll">
+              {reviews.map((review, i) => (
+                <div className="review" key={i}>
+                  <p>{review.name}{'★'.repeat(review.stars)}{'☆'.repeat(5 - review.stars)}</p>
+                  <p>{review.date}</p>
+                  <p>{review.text}</p>
+                </div>
+              ))}
+            </div>
+
+          </div>
         </div>
       </div>
 
