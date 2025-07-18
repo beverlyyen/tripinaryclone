cc5e317ff67ad4c9a41642476f05c23e67b2eeaf
Lilian Pham <lilianlienpham@gmail.com>
2025-07-18 03:45:20 -0700

    added another parameter for activity description

--- src/Tripinary/src/pages/itinerary-page/itinerary.jsx
+++ src/Tripinary/src/pages/itinerary-page/itinerary.jsx
@@ -1,31 +1,56 @@
 import React from "react";
 import DayCard from "./day-card";
 import "./itinerary.css";
-
 const itineraryData = {
-  destination: "Vancouver",
-  days: [
-    { day: "Day 1", items: ["08:00", "09:00", "10:00"] },
-    { day: "Day 2", items: ["08:00", "09:00", "10:00"] },
-    { day: "Day 3", items: ["08:00", "09:00", "10:00"] },
-  ],
+    destination: "Vancouver",
+    days: [
+        {
+            day: "Day 1",
+            items: [
+                { time: "08:00", activity: "Visit Stanley Park" },
+                { time: "09:00", activity: "Explore Gastown" },
+                { time: "10:00", activity: "Coffee at Tim Hortons" },
+            ],
+        },
+        {
+            day: "Day 2",
+            items: [
+                { time: "08:00", activity: "Visit Burnaby Park Conservation" },
+                { time: "09:00", activity: "SFU Mountain Hike" },
+                { time: "10:00", activity: "Lunch at Cactus Club" },
+            ],
+        },
+        {
+            day: "Day 3",
+            items: [
+                { time: "08:00", activity: "Kitsilano Beach Morning Walk" },
+                { time: "09:00", activity: "Yoga by the ocean" },
+                { time: "10:00", activity: "Brunch at Local" },
+            ],
+        },
+    ],
 };
 
 export default function Itinerary() {
-  return (
-    <div className="itinerary-container">
-      <div className="itinerary-header">
-        <h1>Itinerary</h1>
-        <p>Trip to... {itineraryData.destination}</p>
-      </div>
+    return (
+        <div className="itinerary-container">
+            <div className="itinerary-header">
+                <h1>Itinerary</h1>
+                <p>Trip to... {itineraryData.destination}</p>
+            </div>
 
-      {itineraryData.days.map((day, index) => (
-        <DayCard key={index} title={day.day} times={day.items} />
-      ))}
+            {itineraryData.days.map((day, index) => (
+                <DayCard
+                    key={index}
+                    title={day.day}
+                    times={day.items}
+                    place={itineraryData.destination}
+                />
+            ))}
 
-      <div className="itinerary-button">
-        <button className="regenerate-button">Regenerate ↺</button>
-      </div>
-    </div>
-  );
+            <div className="itinerary-button">
+                <button className="regenerate-button">Regenerate ↺</button>
+            </div>
+        </div>
+    );
 }
