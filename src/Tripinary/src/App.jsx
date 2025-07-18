b2a7864cec2eca04e6bf31b9c005438009b14c5d
beverlyyen <beverly.yen@gmail.com>
2025-07-18 14:17:59 -0700

    fixing merge conflicts

--- src/Tripinary/src/App.jsx
+++ src/Tripinary/src/App.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import SidePanel from './pages/slide-out/slideout';
-import './pages/slide-out/slideout.css';
+import './pages/slide-out/slideout.css'; 
+
+import ItineraryPage from './pages/itinerary-page/itinerary';
+import DayCard from './pages/itinerary-page/day-card'; 
+import './App.css'; 
+import Navbar from './pages/home-page/NavBar/navbar';
+import Footer from './pages/home-page/Footer/footer';
+import TripinaryMain from './pages/home-page/TripinaryMain/TripinaryMain';
+import Activity_Suggestions from './pages/activity-suggestions/activity_suggestions'; 
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false); 
 
   return (
-    <div className="app-container">
-      <button className="open-panel-btn" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? 'Close Panel' : 'Open Panel'}
-      </button>
+    <>
+      <Navbar />
+      <TripinaryMain />
+      <ItineraryPage />
+      <Activity_Suggestions /> 
 
-      <SidePanel isOpen={isOpen} />
-    </div>
+    
+      <div className="app-container"> 
+        <button className="open-panel-btn" onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}>
+          {isSidePanelOpen ? 'Close Panel' : 'Open Panel'}
+        </button>
+        <SidePanel isOpen={isSidePanelOpen} />
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
--- src/Tripinary/src/main.jsx
+++ src/Tripinary/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './pages/slide-out/slideout.css';
+import React from 'react'; 
+import { createRoot } from 'react-dom/client'; 
+import './index.css'; 
+import './pages/slide-out/slideout.css'; 
+import App from './App.jsx'; 
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
