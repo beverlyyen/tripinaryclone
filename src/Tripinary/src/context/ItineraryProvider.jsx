8c7a008976a0e219c485407e18606eead768bf8a
beverlyyen <beverly.yen@gmail.com>
2025-07-29 21:58:31 -0700

    ehehehe

--- src/Tripinary/src/context/ItineraryProvider.jsx
+++ src/Tripinary/src/context/ItineraryProvider.jsx
@@ -80,7 +80,7 @@ function ItineraryProvider({ children }) {
   }, [itineraryForm.selectedPlaces]);
 
   const clearItineraryForm = useCallback(() => {
-    setItineraryForm(initialtineraryForm)
+    setItineraryForm(initialtItineraryForm)
     sessionStorage.removeItem('tripinaryItineraryForm');
   }, []);
 
