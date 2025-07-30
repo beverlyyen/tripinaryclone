4bcefd3f8ba24aa3054cf76f0ec993a812677192
beverlyyen <beverly.yen@gmail.com>
2025-07-29 21:10:11 -0700

    Revert "Merge pull request #80 from CMPT-276-SUMMER-2025/itinerary-form-bugfix"
    This reverts commit 32928e38b199acceadab1fa37a4c702d656d3182, reversing
    changes made to 13e9748cbd24d74739f766e80f965f89464813e3.

--- src/Tripinary/src/context/ItineraryProvider.jsx
+++ src/Tripinary/src/context/ItineraryProvider.jsx
@@ -80,7 +80,7 @@ function ItineraryProvider({ children }) {
   }, [itineraryForm.selectedPlaces]);
 
   const clearItineraryForm = useCallback(() => {
-    setItineraryForm(initialItineraryForm); 
+    setItineraryForm(defaultItineraryForm); 
     sessionStorage.removeItem('tripinaryItineraryForm');
   }, []);
 
