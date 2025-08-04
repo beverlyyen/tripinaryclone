f721153b6c9cf2dda0a4fbbaeced77e0d3c74a65
beverlyyen <beverly.yen@gmail.com>
2025-08-04 14:00:08 -0700

    vercel change for proxy

--- src/Tripinary/vite.config.js
+++ src/Tripinary/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://tripinary-one.vercel.app/'
+    }
+  },
   build: {
     outDir: 'build',
   },
